d15de0af39d8804d580191a725f27794e7e94892
Paul Ngisiro <jngisiro@gmail.com>
2018-12-16 12:32:32 +0300

    adding more tests

--- testAPI.py
+++ testAPI.py
@@ -5,12 +5,7 @@ import json
 import sys
 
 TEST_CLIENT = incident.app.test_client()
-
-    
-def test_create_incident():
-    """ Add a new red-flag """
-    response = TEST_CLIENT.post("/api/v1/red_flags/",
-                    data = json.dumps({
+test_data = {
                         "title" : "Theft at UNRA Banda",
                         "createdBy" : 5,
                         "type" : "red-flag",
@@ -19,10 +14,27 @@ def test_create_incident():
                         "images" : ["img/image1.jpg", "img/image2.jpg"],
                         "videos" : ["vid/video1.mp4", "vid/video2.jpg"],
                         "comment" : "It is a long established fact that a reader will be distracted by the readable content of a page"
-                                }), content_type="application/json")                      
-
+                                }
+    
+def test_create_incident():
+    """ Add a new red-flag """
+    response = TEST_CLIENT.post("/api/v1/red_flags/",
+                    data = json.dumps(test_data), content_type="application/json")                      
     assert response.get_json()["data"][0]["message"][:-2] == "Created red-flag record with id"
     assert response.content_type == "application/json"
+
+def test_created_incident():
+    """ Run tests to confirm the incident was created and follows the data rules """
+    response = TEST_CLIENT.get("/api/v1/red_flags/4/")
+    assert response.get_json()["data"]["title"] == test_data["title"]
+    assert response.get_json()["data"]["createdBy"] == test_data["createdBy"]
+    assert response.get_json()["data"]["location"] == test_data["location"]
+    assert response.get_json()["data"]["status"] == test_data["status"]
+    assert response.get_json()["data"]["images"] == test_data["images"]
+    assert response.get_json()["data"]["videos"] == test_data["videos"]
+    assert response.get_json()["data"]["comment"] == test_data["comment"]
+  
+    
 
 def test_get_all_incidents():
     response = TEST_CLIENT.get("/api/v1/red_flags/")
